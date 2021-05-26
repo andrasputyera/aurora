f4ef4a00ead8ef9f0d3795eb7151cfab3b448e48
andrasputyera <andras.putyera@gmail.com>
2021-05-26 17:31:53 +0200

    set up get weather api call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = 'tBRjdhCZYGrp85MgYBSe1FoBFKvVFiOg';
 
+// Get weather information for a specific city
+const getWeather = async(id) => {
+    const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(baseUrl + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// City information
 const getCity = async (city) => {
     const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -10,6 +22,9 @@ const getCity = async (city) => {
     return data[0];
 }
 
-getCity('miami')
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+getCity('miami').then(data => {
+    return getWeather(data.Key)
+}).then(data => {
+    console.log(data)
+})
+.catch(err => console.log(err))
